d6f14ec8adb53651dbaf29fc207124c2170a2320
walehassan <olawalerae15@gmail.com>
2025-02-11 15:02:42 +0100

    Fix line endings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import "../app/globals.css";
 import {Button} from "@radix-ui/themes";
+import { Cloud, Users, ShipWheel, Database } from "lucide-react";
 import { ArrowRightIcon, ArrowLeftIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 
@@ -7,36 +8,39 @@ import Image from "next/image";
 
 const Home = () => {
   return (
-    <div className="container bg-primary">
-      <div className="flex container mx-auto px-10 py-10 md:py-20">
-        <div className="container">
-          <p className="text-white uppercase">Product overview</p>
+    <div className="w-full px-0 bg-primary">
+      {/* Hero */}
+      <div className="flex flex-col md:flex-row gap-12 px-4 md:px-10">
+        <div className="py-10 md:py-20 flex-col md:flex-row">
+          <p className="text-white  uppercase">Product overview</p>
           <h1 className="text-white text-4xl font-bold py-5">
             Elevate Your Business <br /> With Cloud Solutions
           </h1>
         </div>
-        <div>
+        <div className="flex-col md:flex-row py-10 md:py-20 md:px-10">
           <p className="text-white">Our comprehensive cloud services provide scalable, reliable, and secure solutions tailored to your business needs.</p>
-          <div className="flex justify-between py-8">
-            <Button className="bg-accent border text-white py-2 px-4 rounded-full" >On-Demand Demo</Button>
-            <div className="flex text-blue-500">
+          <div className="flex flex-col md:flex-row py-8 gap-8 md:gap-12">
+            <Button className="bg-accent border px-4 md:px-6 text-white bg-primary py-2 rounded-full" >On-Demand Demo</Button>
+            <div className="flex cursor-pointer hover:underline items-center text-blue-500">
               <span className="capitalize">Unlock cloud potential</span>
-              <ArrowRightIcon className="w-5 h-5 align-center"/>
+              <ArrowRightIcon className="w-4 h-4 md:w-5 md:h-5"/>
             </div>
           </div>
         </div>
       </div>
-      <div className="flex justify-center w-full">
+      <div className="flex  justify-center w-full">
         <Image className="w-full" src="/altobg.png" alt="bg image" width={1250} height={200} />
       </div>
-      <div className="container mx-auto px-10 py-20 md:py-20">
+      
+        {/* About Section  */}
+      <div className="py-10 md:px-10 px-4">
         <div className="flex flex-col items-center justify-center text-white w-full h-full">
           <p className="text-center px-10">How It Works</p>
-          <h3 className="text-center text-4xl font-bold px-10">The Magic Behind<br/>AltoCloud</h3>
+          <h3 className="text-center text-2xl md:text-4xl py-5 font-bold px-10">The Magic Behind<br/>AltoCloud</h3>
         </div>
-        <div className="flex justify-between">
+        <div className="flex flex-col md:flex-row gap-4 justify-between">
           <div className="text-white text-bold w-1/2 text-2xl py-10">
-            <h4 className="py-5">01.</h4>
+            <h4 className="py-5 ">01.</h4>
             <h4>
               Elevate Your Business<br/> With Cloud Solutions
             </h4>
@@ -48,25 +52,53 @@ const Home = () => {
             <h4>
               Elevate Your Business<br/> With Cloud Solutions
             </h4>
-            <div className="container text-sm py-10 text-white">
-            <p className="">Elevate Your Business With Cloud Solutions 
+            <div className="px-4 md:px-10 text-sm py-10 text-white">
+              <p className="">Elevate Your Business With Cloud Solutions 
                 Elevate Your Business With Cloud Solutions 
                 Elevate Your Business With Cloud Solutions
               </p>
             </div>
           </div>
-          <div className="py-20">
-            <Image className="rounded-lg px-0 " src="/altobackground.jpg" alt="another section bg" width={500} height={300}/>
+          <div className="w-full py-4">
+            <Image className="w-full rounded-lg" src="/altobackground.jpg" alt="another section bg" width={500} height={300}/>
           </div>
         </div>
       </div>
-      <div className="container mx-auto px-10 py-20 md:py-20">
+
+      {/* Services Section */}
+      <div className="mx-auto px-10 py-20 md:py-20">
         <div className="flex flex-col items-center justify-center text-white w-full h-full">
           <p className="text-center px-10">How We Work</p>
           <h3 className="text-center text-4xl font-bold px-10">What Makes Us Stand<br/>Out</h3>
         </div>
-        <div>
-          
+        <div className="grid grid-cols-2 md:grid-cols-2 gap-4 mt-6 mb-12 divide-500">
+          <div className="flex flex-col border-l-2 border-b-2 py-10 items-center justify-center transition-all duration-300 hover:border-blue-500 hover:shadow-lg hover:shadow-blue-500/50">
+            <Cloud className="w-6 h-6 text-blue-500"/>
+            <p className="text-white">Cloud Migration</p>
+          </div>
+          <div className="flex flex-col border-x-2 border-b-2 py-10 items-center justify-center transition-all duration-300 hover:border-blue-500 hover:shadow-lg hover:shadow-blue-500/50">
+            <ShipWheel className="w-6 h-6 text-blue-500"/>
+            <p className="text-white">DevOps</p>
+          </div>
+          <div className="flex flex-col border-l-2 py-10 items-center justify-center transition-all duration-300 hover:border-blue-500 hover:shadow-lg hover:shadow-blue-500/50">
+            <Users className="w-6 h-6 text-blue-500"/>
+            <p className="text-white">Cloud Security</p>
+          </div>
+          <div className="flex flex-col border-x-2 py-10 items-center justify-center transition-all duration-300 hover:border-blue-500 hover:shadow-lg hover:shadow-blue-500/50">
+            <Database className="w-6 h-6 text-blue-500"/>
+            <p className="text-white">Data Management</p>
+          </div>
+          <div className="w-full flex items-center cursor-pointer hover:underline justify-center text-blue-500">
+              <span className="capitalize">View All Services</span>
+              <ArrowRightIcon className="w-5 h-5 "/>
+          </div>
+        </div>
+      </div>
+
+      {/* Contact Form */}
+      <div>
+        <div className="w-full flex flex-col text-white">
+          <h4 className="text-4xl">Contact Us</h4>
         </div>
       </div>
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,31 @@ import Link from 'next/link';
 import { Nav } from './Nav';
 import Image from 'next/image';
 import { Button } from '@radix-ui/themes';
+import MobileNav  from './MobileNav';
+
+
 
 const Header = () => {
   return (
-    <header className='py-8 xl:py-12 text-white'>  
-      <div className='container mx-auto flex justify-between items-center'>
+    <header className='py-8 xl:py-12 text-white bg-primary'>  
+      <div className='w-full px-4 md:px-10 flex justify-between items-center'>
         <Link className='flex' href='/'>
           <Image src="/altoc.png" alt='altocloud logo' width={50} height={50}/>
           <h1 className="text-4xl font-semibold">
             altoCloud<span className="text-accent">.</span>
           </h1>
         </Link>
-        <Nav/>
-        <Button className='bg-accent border text-white py-2 px-4 rounded-full'>Contact Us</Button>
+        {/* Desktop Navigation */}
+        <div className='hidden xl:flex items-center gap-8'>
+          <Nav />
+        </div>
+        <div className='hidden xl:flex items-center gap-8'><Button className='bg-accent border text-white bg-primary py-2 px-4 rounded-full'>Contact Us</Button></div>
+        {/* Mobile nav */}
+        <div className="xl:hidden">
+          <MobileNav /> 
+        </div>
       </div>
+
     </header>
   )
 }
